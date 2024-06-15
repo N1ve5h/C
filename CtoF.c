14191a4327c15ea72360861704cb299f914e710f
N1ve5h <niveshg@hotmail.com>
2024-06-15 13:24:45 +0100

    ex 1-4

---
+++ CtoF.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    float lower, upper, step;
+    float celsius, fahr;
+    lower = 0;   /* lower limit of temperature scale */
+    upper = 300; /* upper limit */
+    step = 10;   /* step size */
+    celsius = lower;
+    printf("C \t F\n");
+    while (celsius <= upper)
+    {
+        fahr = (9.0 / 5.0 * celsius) + 32.0;
+        printf("%3.0f\t%6.2f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
